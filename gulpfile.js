af31d749eeade34821e4d63a381623920fcf8f33
KhaLe <minhkhanb@gmail.com>
2017-08-18 12:07:35 +0700

    complete workflow

--- gulpfile.js
+++ gulpfile.js
@@ -11,18 +11,26 @@ var gulpIf = require ('gulp-if');
 
 var cssnano = require ('gulp-cssnano');
 
-gulp.task ('default',function () {
-    // place code for your default task here
-});
+var imagemin = require ('gulp-imagemin');
+
+var cache = require ('gulp-cache');
+
+var del = require ('del');
+
+var runSequence = require ('run-sequence');
 
 var src = {
     sass:'app/sass/**/*.+(scss|sass)',
     html:'app/*.html',
-    js:'app/js/**/*.js'
+    js:'app/js/**/*.js',
+    images:'app/images/**/*.+(png|jpg|jpeg|gif|svg)',
+    fonts:'app/fonts/**/*'
 };
 var distination = {
     js:'*.js',
-    css: '*.css'
+    css:'*.css',
+    images:'dist/images',
+    fonts:'dist/images'
 };
 
 gulp.task ('sass',function () {
@@ -47,8 +55,31 @@ gulp.task ('useref',function () {
     .pipe (useref ())     //Use for js file at difference folder
     .pipe (gulpIf (distination.js,uglify ()))
     //Minifies only if it's a CSS file
-    .pipe (gulpIf(distination.css, cssnano()))
+    .pipe (gulpIf (distination.css,cssnano ()))
     .pipe (gulp.dest ('dist'))
+});
+
+gulp.task ('images',function () {
+    return gulp.src (src.images)
+    //Caching images that ran through imagemin
+    .pipe (cache (imagemin ({
+        //Setting interlaced to true
+        interlaced:true //create interlaced GIFs by setting the interlaced option key to true.
+    })))
+    .pipe (gulp.dest (distination.images))
+});
+
+gulp.task ('fonts',function () {
+    return gulp.src (src.fonts)
+    .pipe (gulp.dest (distination.fonts))
+});
+
+gulp.task ('clean',function () {
+    return del.sync ('dist');
+});
+
+gulp.task ('cache:clear',function (callback) {
+    return cache.clearAll (callback)
 });
 
 gulp.task ('watch',['browserSync','sass'],function () {
@@ -56,5 +87,19 @@ gulp.task ('watch',['browserSync','sass'],function () {
     //Reloads the browser whenever HTML or JS files change
     gulp.watch (src.html,browserSync.reload);
     gulp.watch (src.js,browserSync.reload);
+});
+
+gulp.task ('build',function (callback) {
+    runSequence ('clean:dist',
+        ['sass','useref','images','fonts'],
+        callback
+    )
+});
+
+gulp.task ('default',function (callback) {
+    // place code for your default task here
+    runSequence (['sass','browserSync','watch'],
+        callback
+    )
 });
 
